5e0028ade56f55d500982ece20453f9201e0a244
ABdeeglr <abdeeglr@icloud.com>
2023-04-23 22:21:14 +0800

    做了一些简单的测试，看来要重新熟悉一下 java 的语法，现在主要需要解决的问题有三个，分别是字符串的使用；输入和输出；数组的使用

---
+++ HelloGoodbye.java
@@ -0,0 +1,15 @@
+/**
+ * Coursera 课程 week 的起始作业
+ * 本程序为 https://www.coursera.org/learn/algorithms-part1/programming/37IH3/hello-world 上的作业之二（共三个）
+ * https://coursera.cs.princeton.edu/algs4/assignments/hello/specification.php 上描述了该程序需要完成的目标
+ * 
+ * 我现在无法完成它，因为我突然发现我根本不知道如何获取 Java 的数组的长度，也不知道如何打印格式化字符串
+ */
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        // int count = args.length;
+        // System.out.println(" ".format("The parameter number is ", count));
+
+    }
+    
+}
